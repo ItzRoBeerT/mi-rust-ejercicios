bc837ea2309fe1f88cbfc1e4746494e617673830
socrates-ferna <socrates.fernaferna@gmail.com>
2024-09-21 17:50:15 +0200

    Añadir solución para el ejercicio 09 del capítulo 06

---
+++ capitulo06/06_09_socrates-ferna.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut edad: f64 = 30.0;
+    let mut altura: &f32 = &182.00;
+    let inicial_nombre = 'E';
+
+    edad = 32.0;
+    altura = &183.0;
+    let modificador_caracter: &char = &inicial_nombre;
+}
